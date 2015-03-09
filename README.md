ad3f7674fcff854582ae12d1eae12f4180034495
Tobias Baunbæk <freeall@gmail.com>
2015-03-09 14:10:27 +0100

    init

---
+++ README.md
@@ -0,0 +1,17 @@
+# github-info
+
+List information about a github repository
+
+```
+npm install github-info -g
+```
+
+## Usage
+
+```
+$ github-info user/repository   # e.g github-info freeall/github-info
+```
+
+## License
+
+MIT
---
+++ index.js
@@ -0,0 +1,102 @@
+#!/usr/bin/env node
+
+var gh = require('github-with-auth');
+var moment = require('moment');
+
+if (process.argv.length !== 3 || process.argv[2].indexOf('/') === -1) {
+	console.log('Usage:   repo-info user/repository.');
+	console.log('Example: repo-info freeall/repo-info');
+	process.exit(1);
+}
+
+var user = process.argv[2].split('/')[0];
+var repo = process.argv[2].split('/')[1];
+
+gh(['repo'], function(err, github) {
+	info(github, function() {
+		console.log();
+		openPullRequests(github, function() {
+			console.log();
+			closedPullRequests(github, function() {
+				console.log();
+				tags(github, function() { });
+			});
+		});
+	});
+});
+
+var info = function(github, callback) {
+	github.repos.get({
+		user: user,
+		repo: repo
+	}, function(err, repo) {
+		if (err) throw err;
+
+		console.log('General information');
+		console.log('===================');
+		console.log('★  ' + repo.stargazers_count + '  👀  ' + repo.watchers_count + '  🍴  ' + repo.forks);
+		console.log(repo.full_name);
+		console.log('Open issues: ' + repo.open_issues_count);
+		console.log('Is a '+(repo.private ? 'private' : 'public')+ ' repository');
+
+		callback();
+	});
+};
+var printTop = function(type, arr, count, fn) {
+	if (arr.length > count) type += ' ['+count+'/' + arr.length + ']';
+
+	if (!arr.length) return console.log('This repository has no ' + type.toLowerCase());
+
+	console.log(type);
+	console.log(Array(type.length+1).join('='));
+
+	arr.splice(0, count).forEach(fn);
+};
+var tags = function(github, callback) {
+	github.repos.getTags({
+		user: user,
+		repo: repo
+	}, function(err, tags) {
+		if (err) throw err;
+
+		printTop('Latest tags', tags, 5, function(tag) {
+			console.log(tag.name);
+		});
+
+		callback();
+	});
+};
+var openPullRequests = function(github, callback) {
+	github.pullRequests.getAll({
+		user: user,
+		repo: repo,
+		state: 'open',
+		sort: 'created',
+		direction: 'asc'
+	}, function(err, prs) {
+		if (err) throw err;
+
+		printTop('Open pull requests', prs, 5, function(pr) {
+			console.log(pr.title + ' (Created ' + moment(pr.created_at).fromNow() + ')');
+		});
+
+		callback();
+	});
+};
+var closedPullRequests = function(github, callback) {
+	github.pullRequests.getAll({
+		user: user,
+		repo: repo,
+		state: 'closed',
+		sort: 'created',
+		direction: 'desc'
+	}, function(err, prs) {
+		if (err) throw err;
+
+		printTop('Closed pull requests', prs, 5, function(pr) {
+			console.log(pr.title + ' (Closed ' + moment(pr.closed_at).fromNow() + ')');
+		});
+
+		callback();
+	});
+};
